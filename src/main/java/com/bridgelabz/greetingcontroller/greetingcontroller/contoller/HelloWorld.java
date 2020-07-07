855a9d89c55b46c1d73f23213f2b078768f35c9f
gajanan15 <gajanandesai97@gmail.com>
2020-07-08 00:49:56 +0530

    ADDED : Hello World

---
+++ src/main/java/com/bridgelabz/greetingcontroller/greetingcontroller/contoller/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.greetingcontroller.greetingcontroller.contoller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping("/helloworld")
+    public ResponseEntity helloWorld(){
+        return new ResponseEntity("Hello World", HttpStatus.OK);
+    }
+}
